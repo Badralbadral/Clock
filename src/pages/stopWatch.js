6dc9d5caad40fe2e2b4ea281a4c055c801101a8f
Badral <dbadral0220@gmail.com>
2024-01-26 19:48:16 +0900

    clock few changes from home1.26

--- src/pages/stopWatch.js
+++ src/pages/stopWatch.js
@@ -1,56 +1,125 @@
 import { useState } from "react";
 
 export default function StopWatch() {
-  let [msecond, setMsecond] = useState(0 + `0`);
+  let [msecond, setMsecond] = useState(0);
   let [second, setSecond] = useState(0);
   let [minut, setMinut] = useState(0);
-  let [hour, sethour] = useState(0);
+  let [hour, setHour] = useState(0);
+  const [interv, setInterv] = useState();
+  const [begin, setBegin] = useState();
+  const [stoped, setStoped] = useState(false);
 
-  let interval;
-
-  function StopWatch() {
-    interval = setInterval(() => {
-      setMsecond(msecond++);
-      if (msecond > 99) {
-        setSecond((second = second + 1));
-        msecond = 0;
-      } else if (second > 59) {
-        setMinut((minut = minut + 1));
-        second = 0;
-      } else if (minut > 59) {
-        sethour((hour = hour + 1));
-        second = 0;
-      }
-    }, 10);
+  function start() {
+    setInterv(
+      setInterval(() => {
+        setMsecond(msecond++);
+        if (msecond > 99) {
+          setSecond((second = second + 1));
+          setMsecond((msecond = 0));
+        }
+        if (second > 59) {
+          setMinut((minut = minut + 1));
+          setSecond((second = 0));
+        }
+        if (minut > 59) {
+          setHour((hour = hour + 1));
+          setMinut((minut = 0));
+        }
+      }, 10)
+    );
+    setBegin(true);
   }
+
   function stop() {
-    clearInterval(interval);
+    clearInterval(interv);
+    setStoped(true);
+  }
+
+  function reset() {
+    setMsecond(0);
+    setSecond(0);
+    setMinut(0);
+    setHour(0);
+    clearInterval(interv);
+    setBegin(false);
+  }
+
+  function resume() {
+    setStoped(!stoped);
+    setInterv(
+      setInterval(() => {
+        setMsecond(msecond++);
+        if (msecond > 99) {
+          setSecond((second = second + 1));
+          setMsecond((msecond = 0));
+        }
+        if (second > 59) {
+          setMinut((minut = minut + 1));
+          setSecond((second = 0));
+        }
+        if (minut > 59) {
+          setHour((hour = hour + 1));
+          setMinut((minut = 0));
+        }
+      }, 10)
+    );
   }
   return (
     <div className=" h-[424px] mt-2 flex justify-center items-center flex-col text-2xl">
       <p className="text-4xl text-gray-200">
-        {hour} : {minut} : {second} : {msecond}
+        {hour >= 10 ? hour : `0` + hour} : {minut >= 10 ? minut : `0` + minut} :{" "}
+        {second >= 10 ? second : `0` + second} :{" "}
+        {msecond >= 10 ? msecond : `0` + msecond}
       </p>
       <div className="flex gap-5 mt-24">
-        <button
-          onClick={() => {
-            StopWatch();
-          }}
-          className="bg-gray-700 text-gray-300 rounded-lg px-3 py-[2px]"
-        >
-          Start
-        </button>
-        <button
-          onClick={() => {
-            stop();
-          }}
-          className="bg-gray-700 text-gray-300 rounded-lg px-3 py-[2px]"
-        >
-          Stop
-        </button>
-        <button className="bg-gray-700 text-gray-300 rounded-lg px-3 py-[2px]">
-          Reset
-        </button>
+        {!begin ? (
+          <button
+            onClick={() => {
+              start();
+            }}
+            className="bg-gray-700 text-cyan-300 rounded-[50px] px-5 py-[26px] "
+          >
+            Start
+          </button>
+        ) : !stoped ? (
+          <div className="flex gap-8">
+            <button
+              onClick={() => {
+                stop();
+              }}
+              className="bg-gray-700 text-red-400 rounded-[50px] px-5 py-[26px] active:bg-red-500 duration-100"
+            >
+              Stop
+            </button>
+            <button
+              onClick={() => {
+                reset();
+              }}
+              className="bg-gray-700 text-gray-200 rounded-[50px] px-5 py-[26px] active:bg-gray-100 duration-100"
+            >
+              Reset
+            </button>
+          </div>
+        ) : (
+          <div className="flex gap-8">
+            <button
+              onClick={() => {
+                resume();
+              }}
+              className="bg-gray-700 text-cyan-300 rounded-[50px] px-5 py-[26px] "
+            >
+              Resume
+            </button>
+            <button
+              onClick={() => {
+                reset();
+              }}
+              className="bg-gray-700 text-gray-200 rounded-[50px] px-5 py-[26px] active:bg-gray-100 duration-100"
+            >
+              Reset
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/pages/timer.js
+++ src/pages/timer.js
@@ -1,33 +1,126 @@
 import { useState } from "react";
 
 export default function Timer() {
-  const [second, setSecond] = useState(10);
-  const [minut, setMinut] = useState(0);
-  const [hour, sethour] = useState(0);
+  let [second, setSecond] = useState(3);
+  let [minut, setMinut] = useState(0);
+  let [hour, setHour] = useState(1);
+  const [interv, setInterv] = useState();
+  const [begin, setBegin] = useState();
+  const [stoped, setStoped] = useState(false);
 
-  function timer() {
-    setSecond(second - 1);
+  function start() {
+    setInterv(
+      setInterval(() => {
+        setSecond(second--);
+        if (second == -1) {
+          second = 59;
+        }
+        if (second == 58) {
+          setMinut((minut = minut - 1));
+        }
+        // second and minut
+        if (minut == 0) {
+          // setHour((hour = hour - 1));
+          minut = 60;
+        }
+        if (minut == 59 && second == 59) {
+          setHour((hour = hour - 1));
+        }
+        // if (minut < 0) {
+        //   sethour((hour = hour - 1));
+        //   setMinut((minut = 59));
+        // }
+      }, 1000)
+    );
+    setBegin(true);
   }
-  let myTImer;
+
+  function stop() {
+    clearInterval(interv);
+    setStoped(true);
+  }
+
+  function reset() {
+    setSecond(0);
+    setMinut(0);
+    setHour(0);
+    clearInterval(interv);
+    setBegin(false);
+  }
+
+  // function resume() {
+  //   setStoped(!stoped);
+  //   setInterv(
+  //     setInterval(() => {
+  //       setSecond(second--);
+  //       if (second < 0) {
+  //         setMinut((minut = minut - 1));
+  //         second = 59;
+  //       }
+
+  //       // if (minut < 0) {
+  //       //   sethour((hour = hour - 1));
+  //       //   setMinut((minut = 59));
+  //       // }
+  //     }, 1000)
+  //   );
+  // }
 
   return (
-    <div className=" h-[424px] flex gap-24 justify-center items-center flex-col text-2xl mt-2">
+    <div className=" h-[424px] mt-2 flex justify-center items-center flex-col text-2xl">
       <p className="text-4xl text-gray-200">
-        {hour} : {minut} : {second}
+        {hour >= 10 ? hour : `0` + hour} : {minut >= 10 ? minut : `0` + minut} :{" "}
+        {second >= 10 ? second : `0` + second}
       </p>
-      <div className="flex gap-5">
-        <button
-          onClick={(myTImer = setInterval(timer, 1000))}
-          className="bg-gray-700 text-gray-300 rounded-lg px-3 py-[2px]"
-        >
-          Start
-        </button>
-        <button className="bg-gray-700 text-gray-300 rounded-lg px-3 py-[2px]">
-          Stop
-        </button>
-        <button className="bg-gray-700 text-gray-300 rounded-lg px-3 py-[2px]">
-          Pause
-        </button>
+      <div className="flex gap-5 mt-24">
+        {!begin ? (
+          <button
+            onClick={() => {
+              start();
+            }}
+            className="bg-gray-700 text-cyan-300 rounded-[50px] px-5 py-[26px] "
+          >
+            Start
+          </button>
+        ) : !stoped ? (
+          <div className="flex gap-8">
+            <button
+              onClick={() => {
+                stop();
+              }}
+              className="bg-gray-700 text-red-400 rounded-[50px] px-5 py-[26px] active:bg-red-500 duration-100"
+            >
+              Pause
+            </button>
+            <button
+              onClick={() => {
+                reset();
+              }}
+              className="bg-gray-700 text-gray-200 rounded-[50px] px-5 py-[26px] active:bg-gray-100 duration-100"
+            >
+              Cancel
+            </button>
+          </div>
+        ) : (
+          <div className="flex gap-8">
+            <button
+              onClick={() => {
+                resume();
+              }}
+              className="bg-gray-700 text-cyan-300 rounded-[50px] px-5 py-[26px] "
+            >
+              Resume
+            </button>
+            <button
+              onClick={() => {
+                reset();
+              }}
+              className="bg-gray-700 text-gray-200 rounded-[50px] px-5 py-[26px] active:bg-gray-100 duration-100"
+            >
+              Cancel
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
