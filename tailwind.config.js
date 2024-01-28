8d800d285166148280dd11d717421db0211c225c
Badral <dbadral0220@gmail.com>
2024-01-28 12:14:01 +0900

    clock from home 1.28

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+        semi: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(180deg)" },
+        },
+        whole: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+      },
+
+      animation: {
+        semi: "semi 9s linear",
+        whole: "whole 6s linear",
+      },
+    },
   },
   plugins: [],
 };
