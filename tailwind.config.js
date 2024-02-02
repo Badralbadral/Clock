bf17d57c07293cbe4f9b0c664ed969dd43cde57b
Badral Tumursukh <23LP6834@G401-CC0100.local>
2024-02-02 10:48:39 +0800

    wave

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,11 +20,22 @@ module.exports = {
           "0%": { transform: "rotate(0deg)" },
           "100%": { transform: "rotate(360deg)" },
         },
+        wave: {
+          "0%, 100%": {
+            clipPath:
+              "polygon(0 48%, 10% 34%, 26% 30%, 42% 33%, 51% 46%, 72% 54%, 88% 54%, 100% 50%, 100% 100%, 0 100%)",
+          },
+          "50%": {
+            clipPath:
+              "polygon(0 63%, 13% 66%, 26% 70%, 43% 65%, 58% 55%, 70% 44%, 91% 44%, 100% 52%, 100% 100%, 0 100%)",
+          },
+        },
       },
 
       animation: {
         semi: "semi 9s linear",
         whole: "whole 6s linear",
+        wave: "wave 4s ease-in-out infinite",
       },
     },
   },
